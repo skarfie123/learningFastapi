b1c331092a749b41f19c2a19a41d0c24ab6c8719
skarfie123 <rahulpai@rocketmail.com>
2023-05-25 22:34:45 +0100

    enum, path, bool

--- learning_fastapi/main.py
+++ learning_fastapi/main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Union
 
 from fastapi import FastAPI
@@ -18,10 +19,41 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+
+    # short parsed as True if given [true, True, 1, on, yes], case insensitive
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+class ModelName(str, Enum):  # str is needed for docs
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")  # allow paths with `/` with `:path`
+async def read_file(file_path: str):
+    return {"file_path": file_path}
