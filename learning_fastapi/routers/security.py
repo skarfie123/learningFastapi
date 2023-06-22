fd1704224c1159c9e5088e364d2ba24ce2bfe110
skarfie123 <rahulpai@rocketmail.com>
2023-06-22 23:27:43 +0100

    security

--- learning_fastapi/routers/security.py
+++ learning_fastapi/routers/security.py
@@ -1,40 +1,50 @@
-from typing import Annotated
+from datetime import datetime, timedelta
+from typing import Annotated, Any, Literal, Optional
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 from pydantic import BaseModel
 
-router = APIRouter(prefix="/security", tags=["security"])
-
-# requires python-multipart because OAuth2 uses form data for username and password
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="security/token")
+# "python-jose[cryptography]" is used for JWTs
+# "passlib[bcrypt]" is used for password hashing
 
 
-@router.get("/items/")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+DBUser = dict[str, Any]
+DB = dict[str, DBUser]
 
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
         "disabled": False,
     },
     "alice": {
         "username": "alice",
-        "full_name": "Alice Wonderson",
+        "full_name": "Alice",
         "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
         "disabled": True,
     },
 }
 
 
-def fake_hash_password(password: str):
-    return "fakehashed" + password
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
 
 
 class User(BaseModel):
@@ -48,27 +58,73 @@ class UserInDB(User):
     hashed_password: str
 
 
-def get_user(db, username: str):
+router = APIRouter(prefix="/security", tags=["security"])
+
+# context used for password hashing and verifying
+# - schemes specifies the supported algorithms
+# - deprecated indicates which of the schemes should need to be rehashed. see:
+#   https://passlib.readthedocs.io/en/stable/lib/passlib.context.html#context-deprecated-option
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# requires python-multipart because OAuth2 uses form data for username and password
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="security/token")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db: DB, username: str) -> Optional[UserInDB]:
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
 
 
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
+def authenticate_user(
+    fake_db: DB, username: str, password: str
+) -> UserInDB | Literal[False]:
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
     return user
 
 
+def create_access_token(username: str):
+    # https://www.rfc-editor.org/rfc/rfc7519#section-4.1.2
+    data = {"sub": username}
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    # https://www.rfc-editor.org/rfc/rfc7519#section-4.1.4
+    data.update({"exp": expire})
+    encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+    print(f"{encoded_jwt=}")
+    return encoded_jwt
+
+
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},  # this is part of the spec. it
-        )
+    # OWASP recommends not to give specific auth errors so reuse this generic one
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        # this header is part of the http auth spec to indicate what auths are supported
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
     return user
 
 
@@ -80,21 +136,31 @@ async def get_current_active_user(
     return current_user
 
 
-@router.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+@router.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    # pylint: disable-next=no-member
+    access_token = create_access_token(username=user.username)
+    return {"access_token": access_token, "token_type": "bearer"}
 
-    return {"access_token": user.username, "token_type": "bearer"}
 
-
-@router.get("/users/me")
+@router.get("/users/me/", response_model=User)
 async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)]
-) -> User:
+):
     return current_user
+
+
+@router.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
